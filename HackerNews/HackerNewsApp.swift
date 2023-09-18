27cf582cf29b46ce8491cb8d1e085156f1d4a8bd
Kirilloao <kirilltaraturin747@gmail>
2023-09-18 18:40:27 +0200

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Kirill Taraturin on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
