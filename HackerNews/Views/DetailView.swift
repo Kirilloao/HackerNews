8c72f592e9ef0e1e8b500fb995cfb4c101e097be
Kirilloao <kirilltaraturin747@gmail>
2023-09-18 20:37:53 +0200

    done

---
+++ HackerNews/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Kirill Taraturin on 18.09.2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
